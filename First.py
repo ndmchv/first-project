1bb1b0733a42e578bdde559f57c3d5104d940a5f
ndmchv <noemi.csato@pontsystems.eu>
2019-11-06 18:33:30 +0100

    jkgjgfhjfj

---
+++ First.py
@@ -0,0 +1,7 @@
+print(100)
+print(200)
+print("szöveget idézőjelbe kell tenni, számot nem")
+print("az utasításokat külön sorba kell írni")
+print("az utasításokat külön sorba kell írni")
+print("ctrl+d")
+
